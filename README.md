4efab05526f752dec719c705367c8ec33f1333e4
Mindcool25 <Mindcool24@jsociety.xyz>
2022-07-17 15:38:14 -0600

    Added CSS implementation

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # gopher_html
 A wrapper written for the gopher protocol in rust
+
+CSS file written by Xe, found here:
+https://github.com/Xe/Xess
--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,7 @@ use std::io::Read;
 cgi::cgi_main! { |request: cgi::Request| -> cgi::Response {
     let entry = request.headers().get("x-cgi-query-string").unwrap().to_str().expect("Failed");
     let url: Vec<&str> = entry.split('*').collect();
-    let mut page: String;
+    let page: String;
     
     if url[0].is_empty() {
         page = get_gopher_page("192.168.1.154:70".to_string(), "".to_string());
@@ -33,7 +33,7 @@ fn get_gopher_page(ip: String, request: String) -> String {
 
     // Parsing the inputs
     let mut gm: Vec<&str> = in_string.as_str().split('\n').collect();
-    let mut out: String = "".to_string();
+    let mut out: String = "<head><link rel = \"stylesheet\" href=\"../main.css\"></head>".to_string();
     for i in gm.iter_mut() {
         if i.starts_with('i') {
             let line = &i[1..i.len()];
@@ -42,7 +42,7 @@ fn get_gopher_page(ip: String, request: String) -> String {
         else if i.starts_with('0') {
             let line = &i[1..i.len()];
             let split_line: Vec<&str> = line.split('\t').collect();
-            out += &format!("<p><a href=?0*{}*{}:{}>link</a>\t{}</p>",
+            out += &format!("<p><a href=?0*{}*{}:{}>TXT</a> \t{}</p>",
                 split_line[1],
                 split_line[2],
                 split_line[3],
@@ -51,7 +51,7 @@ fn get_gopher_page(ip: String, request: String) -> String {
         else if i.starts_with('1') {
             let line = &i[1..i.len()];
             let split_line: Vec<&str> = line.split('\t').collect();
-            out += &format!("<p><a href=?1*{}*{}:{}>link</a>\t{}</p>",
+            out += &format!("<p><a href=?1*{}*{}:{}>MAP</a> \t{}</p>",
                 split_line[1],
                 split_line[2],
                 split_line[3],
@@ -69,7 +69,7 @@ fn get_text_file(ip: String, request: String) -> String {
     // Reading from server
     let mut buf = [0;32768];
     let _bytes_read = stream.read(&mut buf);
-    let txt = format!("<p>{}</p>", String::from_utf8_lossy(&buf).replace(&['\r', '\u{0}'][..], ""));
-    txt.replace('\n', "<br>")
-
+    let mut out = "<head><link rel = \"stylesheet\" href=\"../main.css\"></head>".to_string();
+    out += &format!("<p>{}</p>", String::from_utf8_lossy(&buf).replace(&['\r', '\u{0}'][..], ""));
+    out.replace('\n', "<br>")
 }
